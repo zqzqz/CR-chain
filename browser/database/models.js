dfc3dbe1a17b6b29d2bbd896f80d351cf42897de
zhengjilai <zhengjilai5816@163.com>
2018-06-02 11:53:27 +0800

    add db of requests

--- browser/database/models.js
+++ browser/database/models.js
@@ -8,7 +8,18 @@ module.exports = {
     owner: {type:String, required:true},
     keyword: {type:String},
     summary: {type:String},
+    uploadTimestamp : {type:String, required:true},
+    addr : {type:String, required:true},
     hash: {type:String, required:true}
+  },
+  requests: {
+    sender: {type:String, required:true},
+    receiver: {type:String, required:true},
+    applyTimestamp: {type:String, required:true},
+    resTimestamp: {type:String, required:true},
+    secret: {type:String},
+    stat: {type:int},
+    fid: {type:String, required:true} 
   }
 };
 
--- browser/routes/index.js
+++ browser/routes/index.js
@@ -16,4 +16,20 @@ router.get('/file', function(req, res, next) {
     })
 });
 
+
+/* GET home page. */
+router.get('/requests', function(req, res, next) {
+    console.log(req.body);
+    var query = req.body;
+    var requests = global.dbHandler.getModel('requests');
+    requests.find(query, function(err, data) {
+        if (err) {
+          // fatal err
+          return res.sendStatus(500);
+        } else {
+          return res.send(data);
+        }
+    })
+});
+
 module.exports = router;
